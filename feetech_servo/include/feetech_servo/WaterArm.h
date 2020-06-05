fddb1c664a1c8e58e3d2fb3a36589ace92b8a977
javi <theceravedi@gmail.com>
2020-06-05 16:09:52 +0200

    Added waterArm class base

---
+++ feetech_servo/include/feetech_servo/WaterArm.h
@@ -0,0 +1,49 @@
+//---------------------------------------------------------------------------------------------------------------------
+//  
+//---------------------------------------------------------------------------------------------------------------------
+//  Copyright 2020 Javier Cebolla Verdugo (ceravedi) theceravedi@gmail.com
+//---------------------------------------------------------------------------------------------------------------------
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//  and associated documentation files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------
+
+#ifndef WATER_ARM_H_
+#define WATER_ARM_H_
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <feetech_servo/SCServo.h>
+
+class WaterArm{
+public:
+    WaterArm();
+
+    bool init(int _idElbow, int _idShoulder);
+    // bool servocalibration();
+    // bool moveElbow(int _position);
+    // bool moveShoulder(int _position);
+
+private:
+    int idElbow_, idShoulder_;
+    int maxPositionElbow_, minPositionElbow_;
+    int maxPositionShoulder_, minPositionShoulder_;
+
+    SCServo *servoDriverElbow_;
+};
+
+
+#endif
